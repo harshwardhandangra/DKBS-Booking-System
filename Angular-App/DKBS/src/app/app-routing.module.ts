0390e6fdbf7fb5fbd3b6f3b70e6439b149aa3e0d
vishaldusane30 <vishaldusane30@gmail.com>
2019-02-06 00:26:46 +0530

    css commit

--- Angular-App/DKBS/src/app/app-routing.module.ts
+++ Angular-App/DKBS/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreatebookingComponent } from './module/booking/createbooking/createbooking.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component:CreatebookingComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular-App/DKBS/src/app/app.module.ts
+++ Angular-App/DKBS/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { CreatebookingComponent } from './module/booking/createbooking/createbooking.component';
 import { HeaderComponent } from './pagescomponent/header/header.component';
 import { FooterComponent } from './pagescomponent/footer/footer.component';
+import { NavigationComponent } from './pagescomponent/navigation/navigation.component';
+import { BookingComponent } from './pages/agent/booking/booking.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreatebookingComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavigationComponent,
+    BookingComponent
   ],
   imports: [
     BrowserModule,
