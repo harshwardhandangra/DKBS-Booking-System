87ee7cb42351181f2cdb0e54c898f84d28aa054c
Ram <kotra.ramakrishna@gmail.com>
2019-02-06 23:13:39 +0530

    dkbs solution created and lands,townzipcodes tables added.

---
+++ Database/dkbs/TownZipCodes.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[TownZipCodes]
+(
+	[TownZipCodeId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ZipPostalCode] NVARCHAR(255) NULL, 
+    [FullInfo] NVARCHAR(255) NULL, 
+    [LandId] INT NULL, 
+    [TownCityName] NVARCHAR(255) NULL, 
+    [LastModified] DATETIME NULL, 
+    [LastModifiedBY] NVARCHAR(255) NULL,
+	CONSTRAINT FK_LandTownZipCodes FOREIGN KEY ([LandId]) REFERENCES Lands([LandId])
+)
