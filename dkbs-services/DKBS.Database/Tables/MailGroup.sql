06e69865dee3c2d19fc900a3d77f45598dbe4666
ranjith@9353 <ranjith9353@gmail.com>
2019-04-27 18:16:18 +0530

    in mailgroup  table feilds are added

--- dkbs-services/DKBS.Database/Tables/MailGroup.sql
+++ dkbs-services/DKBS.Database/Tables/MailGroup.sql
@@ -4,6 +4,8 @@
     [MailGroupsTitle] NVARCHAR(255) NOT NULL, 
     [InternalName] NVARCHAR(255) NULL, 
     [IncludeInPartnerEmail] BIT NULL, 
+	[CreatedDate] DATETIME NULL,
+	[CreatedBy] NVARCHAR(255) NULL,
     [LastModified] DATETIME NOT NULL, 
     [LastModifiedBY] NVARCHAR(255) NOT NULL
 )
