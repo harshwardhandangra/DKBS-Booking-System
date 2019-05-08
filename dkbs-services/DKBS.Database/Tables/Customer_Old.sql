c63f5cd321af387b7f00804b242e057be0574659
RAGHURAJU B <raghuraj.bashetty@gmail.com>
2019-05-08 23:01:22 +0530

    Revert "Revert "Modified Customer and contactPerson tables as per CRM tables and renamed old tables""
    This reverts commit b94fe06fc7861c77e2911cc8d733c49e9c1856fe.

---
+++ dkbs-services/DKBS.Database/Tables/Customer_Old.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Customer_old]
+(
+	[CustomerId] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[CustomerName] [nvarchar](255) NULL,
+	[EmailId] [nvarchar](255) NULL,
+	[PhoneNumber] [nvarchar](255) NULL,
+	[IndustryCode] [nvarchar](1000) NULL,
+	[ZipCode] [nvarchar](100) NULL,
+	[Country] [nvarchar](200) NULL,
+	[City] [nvarchar](200) NULL,
+	[CreatedBy] [nvarchar](200) NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	[LastModifiedDate] [datetime] NOT NULL,
+	[LastModifiedBY] [nvarchar](255) NOT NULL,
+	[MapId] [int] NULL
+)
