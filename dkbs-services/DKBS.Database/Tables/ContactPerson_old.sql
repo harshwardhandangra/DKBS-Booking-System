c7b809221fc40a2db29cd388f91f44ec3531191c
RAGHURAJU B <raghuraj.bashetty@gmail.com>
2019-05-08 22:19:35 +0530

    Modified Customer and contactPerson tables as per CRM tables and renamed old tables

---
+++ dkbs-services/DKBS.Database/Tables/ContactPerson_old.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[ContactPerson_old]
+(
+	[ContactPersonId] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](255) NULL,
+	[Department] [nvarchar](255) NULL,
+	[Position] [nvarchar](255) NULL,
+	[Email] [nvarchar](255) NULL,
+	[Telephone] [nvarchar](50) NULL,
+	[CustomerId] [int] NULL,
+	[MapId] [int] NULL,
+	[CreatedDate] [datetime] NULL,
+	[CreatedBy] [nvarchar](100) NULL,
+	[LastModified] [datetime] NULL,
+	[LastModifiedBy] [nvarchar](100) NULL
+)
