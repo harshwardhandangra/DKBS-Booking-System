b94fe06fc7861c77e2911cc8d733c49e9c1856fe
RAGHURAJU B <raghuraj.bashetty@gmail.com>
2019-05-08 23:00:39 +0530

    Revert "Modified Customer and contactPerson tables as per CRM tables and renamed old tables"
    This reverts commit c7b809221fc40a2db29cd388f91f44ec3531191c.

--- dkbs-services/DKBS.Database/Tables/ContactPerson.sql
+++ dkbs-services/DKBS.Database/Tables/ContactPerson.sql
@@ -1,11 +1,15 @@
 ﻿CREATE TABLE [dbo].[ContactPerson]
 (
 	[ContactPersonId] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	[FirstName] [nvarchar](255) NOT NULL,
-	[LastName] [nvarchar](255) NOT NULL,
-	[Email] [nvarchar](255) NOT NULL,
-	[Telephone] [nvarchar](50) NOT NULL,
-	[MobilePhone] NVARCHAR(50) NOT NULL, 
-    [AccountId] NVARCHAR(255) NOT NULL, 
-    [ContactId] NVARCHAR(255) NOT NULL
+	[Name] [nvarchar](255) NULL,
+	[Department] [nvarchar](255) NULL,
+	[Position] [nvarchar](255) NULL,
+	[Email] [nvarchar](255) NULL,
+	[Telephone] [nvarchar](50) NULL,
+	[CustomerId] [int] NULL,
+	[MapId] [int] NULL,
+	[CreatedDate] [datetime] NULL,
+	[CreatedBy] [nvarchar](100) NULL,
+	[LastModified] [datetime] NULL,
+	[LastModifiedBy] [nvarchar](100) NULL
 )
--- dkbs-services/DKBS.Database/Tables/Customer.sql
+++ dkbs-services/DKBS.Database/Tables/Customer.sql
@@ -1,14 +1,16 @@
 ﻿CREATE TABLE [dbo].[Customer]
 (
 	[CustomerId] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	[CompanyName] [nvarchar](255) NOT NULL,
-	[Address1] [nvarchar](255) NOT NULL,
-	[Address2] [nvarchar](255) NOT NULL,
-	[Town] [nvarchar](200) NOT NULL,
-	[PostCode] [nvarchar](100) NOT NULL,
-	[PhoneNumber] [nvarchar](255) NOT NULL,
-	[Country] [nvarchar](200) NOT NULL,
-	[StateAgreement] BIT NOT NULL,
-	[AccountId] NVARCHAR(255) NOT NULL,
-	[IndustryCode] NVARCHAR(1000) NOT NULL
+	[CustomerName] [nvarchar](255) NULL,
+	[EmailId] [nvarchar](255) NULL,
+	[PhoneNumber] [nvarchar](255) NULL,
+	[IndustryCode] [nvarchar](1000) NULL,
+	[ZipCode] [nvarchar](100) NULL,
+	[Country] [nvarchar](200) NULL,
+	[City] [nvarchar](200) NULL,
+	[CreatedBy] [nvarchar](200) NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	[LastModifiedDate] [datetime] NOT NULL,
+	[LastModifiedBY] [nvarchar](255) NOT NULL,
+	[MapId] [int] NULL
 )
