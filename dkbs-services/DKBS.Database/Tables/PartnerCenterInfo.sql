8ede83631159038e74d4ecf66fb35c3330b2d449
ranjith@9353 <ranjith9353@gmail.com>
2019-05-08 16:41:50 +0530

    Missing Partner Tables Are Created

---
+++ dkbs-services/DKBS.Database/Tables/PartnerCenterInfo.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[PartnerCenterInfo]
+(
+	[PartnerCenterInfo_Id] INT NOT NULL PRIMARY KEY, 
+    [Total Rooms] INT NULL, 
+    [Group Rooms] INT NULL, 
+    [Max space at row of chairs] NVARCHAR(50) NULL, 
+    [Max space at tables] NVARCHAR(50) NULL, 
+    [State agreement] BIT NULL, 
+    [Max Accommodation] NVARCHAR(50) NULL, 
+    [PartnerId] INT NULL,
+	CONSTRAINT [FK_PartnerCenterInfo_Partner] FOREIGN KEY ([PartnerId]) REFERENCES [Partner](PartnerId),
+)
